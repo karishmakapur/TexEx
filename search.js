e37cf7314d0bc09cfb0268051f7cc3593dc4c688
karishmakapur <54446875+karishmakapur@users.noreply.github.com>
2020-10-20 16:17:14 -0700

    nav bar updated

--- search.js
+++ search.js
@@ -1,5 +1,8 @@
 
 function searchBook(event){
+	
+	deleteResults(); //clearing search results, if there are any
+	
 	var searchOption = document.getElementById("searchType").value;
 	console.log(searchOption);
 	
@@ -15,66 +18,113 @@ function searchBook(event){
 	//results[0][3] will be first book ISBN, 
 	//results[0][4] will be first book description, 
 	var results = new Array(new Array());
-	//if no results, say no results
-	results = [["image","title", "isbn", "author", "desc"],["image","title", "isbn", "author", "desc"],["image","title", "isbn", "author", "desc"]];
-	//the following code is for no results.
-	if(results.length == 0){
-		var div = document.createElement("div");
-		div.innerHTML = '<div id="results"> <p> No results </p> </div>';
+	
+	//testing array for search with actual results. This will be changes to the return 2D array of a PHP function.
+	//to test with no results, comment out the below line and run it.
+	results = [["Images/samplePic.svg","title", "isbn", "author", "desc"],["Images/samplePic.svg","title", "isbn", "author", "desc"],["Images/samplePic.svg","title", "isbn", "author", "desc"]];
+	console.log(isEmpty(results));
+	
+	//if there are no results
+	if(isEmpty(results)){
 		
-		document.getElementById("searchForm").insertAdjacentHTML('afterend', '<br />');
-		div.style.margin = "auto";
-		div.style.position = "relative";
-		div.style.top = "100px";
-		div.style.textAlign = "center";
-		div.style.border = "2px solid red";
-		document.getElementById("searchForm").insertAdjacentElement('afterend', div);
+		//create elements
+		var searchResultContainer = document.createElement("div");
+		var noResultsPara = document.createElement("p");
+		
+		//set paragraph
+		noResultsPara.setAttribute("class", "noresults");
+		noResultsPara.textContent = "No results! Try another search term.";
+		
+		//set div container
+		searchResultContainer.setAttribute("id", "resultsOfSearch");
+		searchResultContainer.setAttribute("class", "searchResult");
+		
+		//append para to div
+		searchResultContainer.appendChild(noResultsPara);
+		
+		//add div to document.
+		//append searchResultContainer to end of document.
+		document.getElementById("searchWrapper").insertAdjacentElement('afterend', searchResultContainer);
 	}
 	else{
+		//if there are results
 		for(var i = 0; i < results.length; i++){
 				
 				var result = results[i];
 				console.log("result is", result);
-				var div = document.createElement("div");
-				var img = document.createElement("img");
-				var innerDiv = document.createElement("div");
-				var titlePara = document.createElement("p");
-				var ISBNPara = document.createElement("p");
-				var authorPara = document.createElement("p");
-				var descPara = document.createElement("p");
 				
-				div.innerHTML = '<div id="results"></div>';
-				img.className = "bookPic";
-				img.setAttribute("src","Images/samplePic.svg"); //this will be the image results[i][0]
-				img.setAttribute("alt", "Book Picture");
-				img.setAttribute("width", "100");
-				img.setAttribute("height", "100");
+				var searchResultContainer = document.createElement("div");
+				var bookImage = document.createElement("img");
+				var subDiv = document.createElement("div");
+				var titleLabel = document.createElement("label");
+				var titleInput = document.createElement("input");
+				var authorLabel = document.createElement("label");
+				var authorInput = document.createElement("input");
+				var isbnLabel = document.createElement("label");
+				var isbnInput = document.createElement("input");
+				var descInput = document.createElement("input");
+				var descLabel = document.createElement("p");
 				
-				innerDiv.innerHTML = '<div id="BookInfo"/></div>';
-				
-				titlePara.className = "textarea";
-				titlePara.innerHTML = result[1] + "&nbsp;<br/>";
-				ISBNPara.className = "textarea";
-				ISBNPara.innerHTML = result[2] + "&nbsp;<br/>";
-				authorPara.className = "textarea";
-				authorPara.innerHTML = result[3] + "&nbsp;<br/>";
-				descPara.className = "textarea";
-				descPara.innerHTML = result[4] + "&nbsp;<br/>";
+				//inner most elements: input fields
+				titleInput.setAttribute("type", "text");
+				titleInput.setAttribute("class", "row1");
+				titleInput.setAttribute("disabled", "true");
+				titleInput.setAttribute("value", result[1]);
 				
 				
-				innerDiv.appendChild(titlePara);
-				innerDiv.appendChild(ISBNPara);
-				innerDiv.appendChild(authorPara);
-				innerDiv.appendChild(descPara);
+				isbnInput.setAttribute("type", "text");
+				isbnInput.setAttribute("class", "row1");
+				isbnInput.setAttribute("disabled", "true");
+				isbnInput.setAttribute("value", result[2]);
 				
+				authorInput.setAttribute("type", "text");
+				authorInput.setAttribute("class", "row1");
+				authorInput.setAttribute("disabled", "true");
+				authorInput.setAttribute("value", result[3]);
 				
-				div.className = "searchResult";
+				//surrounding input fields: labels
+				titleLabel.setAttribute("class", "line");
+				authorLabel.setAttribute("class", "line");
+				isbnLabel.setAttribute("class", "line");
 				
-				div.appendChild(img);
-				insertAfter(innerDiv, img);
-			
-				document.getElementById("searchForm").insertAdjacentElement('afterend', div);
-
+				//place input fields inside labels
+				titleLabel.appendChild(titleInput);
+				titleLabel.appendChild(document.createTextNode("Title"));
+				isbnLabel.appendChild(isbnInput);
+				isbnLabel.appendChild(document.createTextNode("ISBN"));
+				authorLabel.appendChild(authorInput);
+				authorLabel.appendChild(document.createTextNode("Author"));
+				
+				//other input field
+				descInput.setAttribute("type", "text");
+				descInput.setAttribute("disabled", "true");
+				descInput.setAttribute("class", "row2");
+				descInput.setAttribute("value", result[4]);
+				descLabel.setAttribute("class", "line2");
+				descLabel.textContent = "Description/Contact Information";
+				
+				//place labels and input field inside subDiv
+				subDiv.setAttribute("class", "innerDiv");
+				subDiv.appendChild(titleLabel);
+				subDiv.appendChild(authorLabel);
+				subDiv.appendChild(isbnLabel);
+				subDiv.appendChild(descInput);
+				subDiv.appendChild(descLabel);
+				
+				//image
+				bookImage.setAttribute("class", "bookPic");
+				bookImage.setAttribute("src", result[0]);
+				bookImage.setAttribute("alt", "Book Picture");
+				
+				//place subDiv and image inside searchResultContainer
+				searchResultContainer.setAttribute("id", "resultsOfSearch");
+				searchResultContainer.setAttribute("class", "searchResult");
+				searchResultContainer.appendChild(bookImage);
+				searchResultContainer.appendChild(subDiv);
+				
+				//append searchResultContainer to end of document.
+				document.getElementById("searchWrapper").insertAdjacentElement('afterend', searchResultContainer);
+				
 		}
 		
 	}
@@ -84,6 +134,12 @@ function saveSearch(event){
 	
 }
 
-function insertAfter(el, referenceNode) {
-    referenceNode.parentNode.insertBefore(el, referenceNode.nextSibling);
+ function deleteResults() { 
+		while(document.contains(document.getElementById("resultsOfSearch"))) {
+				document.getElementById("resultsOfSearch").remove();
+		}   
+ }
+ 
+ function isEmpty(array) {
+  return Array.isArray(array) && (array.length == 0 || array.every(isEmpty));
 }
