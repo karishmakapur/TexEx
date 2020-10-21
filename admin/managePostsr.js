9252235a608cf814f709f605963a1743f4482e81
Marcos Lopez <71795774+lopez816@users.noreply.github.com>
2020-10-20 21:59:17 -0700

    edit/save buttons need to be labeled in .js file

---
+++ admin/managePostsr.js
@@ -0,0 +1,5 @@
+var domEditRow = document.getElementById("editButton");
+var domSavesRow = document.getElementById("saveButton");
+
+domsEditRow.addEventListener("click", editRow, false);
+domSavesRow.addEventListener("change", saveRow, false);
