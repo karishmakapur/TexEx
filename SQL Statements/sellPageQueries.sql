fb869a7519b9ef79a1f9a45389e7b0a4467df323
karishmakapur <54446875+karishmakapur@users.noreply.github.com>
2020-11-11 11:55:50 -0800

    sell page queries

---
+++ SQL Statements/sellPageQueries.sql
@@ -0,0 +1,12 @@
+/*query for making a post*/
+/*this query is dependent upon input from the user, 
+so for now the values are hardcoded into the statement*/
+
+INSERT INTO tbl_book_post(UserID, BookTitle, BookAuthor, BookISBN, PostContent, BookImage, PostedStamp)
+VALUES ((SELECT USERID FROM tbl_user WHERE Email LIKE 'kapur004@cougars.csusm.edu'),'Give Me Liberty! An American History', 'Eric Foner', '978-0393418262', 'This is an economics textbook. Contact me at (222)222-2222', 'Economics.PNG', now());
+
+
+/*query for getting all posts sorted by most recently posted*/
+SELECT BookTitle, BookAuthor, BookISBN, PostContent, BookImage
+FROM tbl_book_post
+ORDER BY PostedStamp DESC;
