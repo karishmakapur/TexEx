7578a71606ccfcf8ede039839d44ef9ca5d44afe
karishmakapur <54446875+karishmakapur@users.noreply.github.com>
2020-11-11 20:11:08 -0800

    accountInfo page queries

--- SQL Statements/accountInfoQueries.sql
+++ SQL Statements/accountInfoQueries.sql
@@ -1,39 +1,27 @@
-SELECT  Name, Email, School FROM tbl_user
-WHERE UserID = '1'; 
+/*Query to get a user's account information for the My Account Information section*/
+SELECT  Name, Email, School 
+FROM tbl_user
+WHERE UserID = (SELECT UserID FROM tbl_user WHERE Email LIKE 'lopez816@cougars.csusm.edu');
 
 /* update user information */
 UPDATE tbl_user 
-SET Name = 'Marco', Email = 'lopez811@csu.fullerton.edu', School = 'California State University Fullerton'
-WHERE UserID = '1';
+SET Name = 'Marco', Email = 'lopez811@csu.fullerton.edu', School = 'California State University Fullerton', UpdatedDate = '2020-11-11 07:51:09'
+WHERE UserID = (SELECT x.UserID FROM (SELECT UserID FROM tbl_user WHERE Email LIKE 'lopez816@cougars.csusm.edu') as x);
 
-/* display updated user information */
-SELECT  Name, Email, School FROM tbl_user
-WHERE UserID = '1';
-
+/*get user's current password for password change validation*/
 SELECT password FROM tbl_user
-WHERE UserID = '1';
+WHERE UserID = (SELECT UserID FROM tbl_user WHERE Email LIKE 'lopez811@csu.fullerton.edu');
 
-/* update password */
+/* if user entered in the correct old password, then we can update password */
 UPDATE tbl_user 
-SET password = 'P@ssword11'
-WHERE UserID = '1';
+SET password = 'n3wP@sswordM@rco', UpdatedDate = '2020-11-11 08:00:00'
+WHERE UserID = (SELECT x.UserID FROM (SELECT UserID FROM tbl_user WHERE Email LIKE 'lopez811@csu.fullerton.edu') as x);
 
-/* display updated user password */
-SELECT password FROM tbl_user
-WHERE UserID = '1';
-
-SELECT Disabled, DisabledDate FROM tbl_user
-WHERE UserID = '1';
-
-/* update account status */
+/* update account status to disabled */
 UPDATE tbl_user 
-SET Disabled = TRUE, DisabledDate = CURRENT_TIMESTAMP
-WHERE UserID = '1';
-
-/* display updated account status */
-SELECT Disabled, DisabledDate FROM tbl_user
-WHERE UserID = '1';
+SET Disabled = TRUE, DisabledDate = '2020-11-11 08:01:59'
+WHERE UserID = (SELECT x.UserID FROM (SELECT UserID FROM tbl_user WHERE Email LIKE 'lopez811@csu.fullerton.edu') as x);
 
 /* display all saved searches in the account page */
 SELECT SearchType, SearchTerm FROM tbl_saved_search
-WHERE UserID = '1';
+WHERE UserID = (SELECT x.UserID FROM (SELECT UserID FROM tbl_user WHERE Email LIKE 'lopez811@csu.fullerton.edu') as x);
--- SQL Statements/myPostQueries.sql
+++ SQL Statements/myPostQueries.sql
@@ -6,12 +6,12 @@ ORDER BY PostedStamp DESC;
 
 /*Query for editing a post*/
 UPDATE tbl_book_post
-SET BookTitle = "Economics Book", BookAuthor = "Mankiw", BookISBN = '123-4567891234', PostContent = 'I have changed the content of this post.'
-WHERE UserID = (SELECT UserID FROM tbl_user WHERE tbl_user.Email LIKE 'sebas004@cougars.csusm.edu')
-AND PostID = 1;
+SET BookTitle = "Programming Book", BookAuthor = "R. Sebesta", BookISBN = '123-4567891234', PostContent = 'I have changed the content of this post.'
+WHERE UserID = (SELECT UserID FROM tbl_user WHERE tbl_user.Email LIKE 'lopez816@cougars.csusm.edu')
+AND PostID = 2;
 
 /*Query for deleting a post*/
 UPDATE tbl_book_post
 SET PostVisible = FALSE, LastUpdated = '2020-11-11 07:47:35'
-WHERE UserID = (SELECT UserID FROM tbl_user WHERE tbl_user.Email LIKE 'knight2112@csu.fullerton.edu')
-AND PostID = 9;
+WHERE UserID = (SELECT UserID FROM tbl_user WHERE tbl_user.Email LIKE 'lopez816@cougars.csusm.edu')
+AND PostID = 2;
