9d0a5436244e3737d6bb42ef83b67395ff452c32
karishmakapur <54446875+karishmakapur@users.noreply.github.com>
2020-11-11 18:13:36 -0800

    new tables

--- SQL Statements/CreateTexExTables.sql
+++ SQL Statements/CreateTexExTables.sql
@@ -1,26 +1,46 @@
+DROP TABLE IF EXISTS tbl_reviews;
 DROP TABLE IF EXISTS tbl_book_post;
 DROP TABLE IF EXISTS tbl_saved_search;
+DROP TABLE IF EXISTS tbl_manages;
+DROP TABLE IF EXISTS tbl_admin;
 DROP TABLE IF EXISTS tbl_user;
 
 CREATE TABLE tbl_user (
 UserID int NOT NULL AUTO_INCREMENT UNIQUE,
 Name varchar(255) NOT NULL,
 Email varchar(255) NOT NULL,
-School varchar(255) NOT NULL,
 Password varchar(255) NOT NULL,
+UpdatedDate datetime,
+School varchar(255) NOT NULL,
 Disabled bool NOT NULL,
 Locked bool NOT NULL,
 CreationDate datetime NOT NULL,
 DisabledDate datetime,
-LockedDate datetime,
-UpdatedDate datetime,
-Role varchar(6) NOT NULL,
-AdminID int,
-CONSTRAINT pk_user PRIMARY KEY (UserID),
-CONSTRAINT fk_admin FOREIGN KEY (AdminID) REFERENCES tbl_user(UserID) ON DELETE SET NULL
+CONSTRAINT pk_user PRIMARY KEY (UserID)
 );
 
 ALTER TABLE tbl_user AUTO_INCREMENT = 0;
+
+CREATE TABLE tbl_admin (
+AdminID int NOT NULL AUTO_INCREMENT UNIQUE,
+Name varchar(255) NOT NULL,
+Email varchar(255) NOT NULL,
+Password varchar(255) NOT NULL,
+CreationDate datetime NOT NULL,
+CONSTRAINT pk_admin PRIMARY KEY (AdminID)
+);
+
+ALTER TABLE tbl_admin AUTO_INCREMENT = 0;
+
+CREATE TABLE tbl_manages (
+AdminID int NOT NULL,
+UserID int NOT NULL,
+ActionTaken varchar(7),
+ActionDate datetime,
+CONSTRAINT pk_manages PRIMARY KEY (AdminID, UserID),
+CONSTRAINT fk_manages_admin FOREIGN KEY (AdminID) REFERENCES tbl_admin(AdminID) ON DELETE CASCADE,
+CONSTRAINT fk_manages_user FOREIGN KEY (UserID) REFERENCES tbl_user(UserID) ON DELETE CASCADE
+);
 
 CREATE TABLE tbl_book_post (
 PostID int NOT NULL AUTO_INCREMENT UNIQUE,
@@ -32,10 +52,9 @@ PostContent longtext NOT NULL,
 BookImage varchar(255) NOT NULL,
 PostedStamp datetime NOT NULL,
 LastUpdated datetime,
-UpdatedBy int,
-CONSTRAINT pk_post PRIMARY KEY (PostID),
-CONSTRAINT fk_postedBy FOREIGN KEY (UserID) REFERENCES tbl_user(UserID) ON DELETE CASCADE,
-CONSTRAINT fk_editedBy FOREIGN KEY (UpdatedBy) REFERENCES tbl_user(UserID) ON DELETE SET NULL
+PostVisible bool,
+CONSTRAINT pk_post PRIMARY KEY (PostID, UserID),
+CONSTRAINT fk_postedBy FOREIGN KEY (UserID) REFERENCES tbl_user(UserID) ON DELETE CASCADE
 );
 
 ALTER TABLE tbl_book_post AUTO_INCREMENT = 0;
@@ -50,3 +69,14 @@ CONSTRAINT fk_savedBy FOREIGN KEY (UserID) REFERENCES tbl_user(UserID) ON DELETE
 );
 
 ALTER TABLE tbl_saved_search AUTO_INCREMENT = 0;
+
+CREATE TABLE tbl_reviews (
+AdminID int NOT NULL,
+PostID int NOT NULL,
+UserID int NOT NULL,
+ActionTaken varchar(7),
+ActionDate datetime,
+CONSTRAINT pk_manages PRIMARY KEY (AdminID, PostID, UserID),
+CONSTRAINT fk_reviews_admin FOREIGN KEY (AdminID) REFERENCES tbl_admin(AdminID) ON DELETE CASCADE,
+CONSTRAINT fk_reviews_post FOREIGN KEY (PostID, UserID) REFERENCES tbl_book_post(PostID, UserID) ON DELETE CASCADE
+);
