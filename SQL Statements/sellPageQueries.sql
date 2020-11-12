5222403d8d89e3e6004f76e0e00271dfbbad0ec0
karishmakapur <54446875+karishmakapur@users.noreply.github.com>
2020-11-11 19:27:24 -0800

    sell page queries
    sell page shows all books and allows a user to make a post.

--- SQL Statements/sellPageQueries.sql
+++ SQL Statements/sellPageQueries.sql
@@ -1,12 +1,11 @@
 /*query for making a post*/
 /*this query is dependent upon input from the user, 
 so for now the values are hardcoded into the statement*/
-
-INSERT INTO tbl_book_post(UserID, BookTitle, BookAuthor, BookISBN, PostContent, BookImage, PostedStamp)
-VALUES ((SELECT USERID FROM tbl_user WHERE Email LIKE 'kapur004@cougars.csusm.edu'),'Give Me Liberty! An American History', 'Eric Foner', '978-0393418262', 'This is an economics textbook. Contact me at (222)222-2222', 'Economics.PNG', now());
+INSERT INTO tbl_book_post(UserID, BookTitle, BookAuthor, BookISBN, PostContent, BookImage, PostedStamp, LastUpdated, PostVisible)
+VALUES ((SELECT USERID FROM tbl_user WHERE Email LIKE 'kapur004@cougars.csusm.edu'),'Give Me Liberty! An American History', 'Eric Foner', '978-0393418262', 'This is an economics textbook. Contact me at (222)222-2222', 'Images/GiveMeLiberty.PNG', now(), NULL, TRUE);
 
 
 /*query for getting all posts sorted by most recently posted*/
-SELECT BookTitle, BookAuthor, BookISBN, PostContent, BookImage
+SELECT BookImage, BookTitle, BookISBN, BookAuthor, PostContent
 FROM tbl_book_post
 ORDER BY PostedStamp DESC;
