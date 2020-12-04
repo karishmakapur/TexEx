9ffe31147726469da893d2aa54776b8b0b58ea49
karishmakapur <54446875+karishmakapur@users.noreply.github.com>
2020-12-04 09:30:25 -0800

    cookie fixes

--- logout.php
+++ logout.php
@@ -1,6 +1,6 @@
 <?php
+	session_set_cookie_params(30 * 60, "/; samesite=Strict", $_SERVER['HTTP_HOST'], 1, 0);
 	session_start();
-	ini_set('session.cookie_secure', 1);
 	$_SESSION['sid'] = NULL;
 	if($_SESSION['sid'] == NULL || $_SESSION['sid'] == 'disabled' ){
 		header("location:login.php");
